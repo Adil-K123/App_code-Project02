85ce489f8e2c857995cd7a650e482088db77ff4c
adilk <adilk@ubuntu-main.myguest.virtualbox.org>
2024-07-14 13:15:39 +0530

    added app_deploy.sh

---
+++ app_compose_files/app_deploy.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+
+#Set the latest image name and tag in the Docker compose file
+sed -E -i 's/image: [^ ]+\.dkr\.ecr/image: $AWS_ACCOUNT_ID\.dkr\.ecr/' 'app_compose_files/docker-compose-$ENVIRONMENT.yaml'
+sed -E -i'' "s/(.*amazonaws.com\/product_review:).*/\1$PRODUCT_REVIEW_VERSION-$ENVIRONMENT/" 'app_compose_files/docker-compose-$ENVIRONMENT.yaml' 
+
+
+#Deply the latest stack
+sudo docker stack deploy --with-registry-auth --detach=false -c app_compose_files/docker-compose-$ENVIRONMENT.yaml app_stack
